81ccd632e8ba95c9dd9864fac5a97dbc55726479
Colin Dellow <cldellow@gmail.com>
2018-01-08 17:50:24 -0500

    Implement index

--- format/src/main/java/com/cldellow/manu/format/Index.java
+++ format/src/main/java/com/cldellow/manu/format/Index.java
@@ -1,6 +1,56 @@
 package com.cldellow.manu.format;
 
+import java.sql.*;
+
 public class Index {
-    public Index(String file, boolean readOnly) {
+    private Connection conn = null;
+
+    public Index(String file, boolean readOnly) throws SQLException {
+        String maybeRo = "";
+        if(readOnly)
+            maybeRo = "?mode=ro";
+
+        conn = DriverManager.getConnection("jdbc:sqlite:file:" + file + maybeRo);
+
+        if(!readOnly)
+            ensureSchema();
+    }
+
+    public void close() throws SQLException {
+      if(conn != null) {
+          conn.close();
+          conn = null;
+      }
+    }
+
+    public int get(String key) throws SQLException {
+      PreparedStatement statement = conn.prepareStatement("SELECT rowid FROM keys WHERE key = ?");
+      try {
+          int rv = -1;
+          statement.setString(1, key);
+          ResultSet rs = statement.executeQuery();
+          if(rs.next())
+              rv = rs.getInt(1);
+          return rv;
+      } finally {
+          statement.close();
+      }
+    }
+
+    public int add(String key) throws SQLException {
+        PreparedStatement statement = conn.prepareStatement("INSERT OR IGNORE INTO keys VALUES (?)");
+        try {
+            statement.setString(1, key);
+            statement.executeUpdate();
+            return get(key);
+        } finally {
+            statement.close();
+        }
+    }
+
+    private void ensureSchema() throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS keys(key TEXT PRIMARY KEY);");
+        statement.close();
     }
 }
--- format/src/test/java/com/cldellow/manu/format/IndexTest.java
+++ format/src/test/java/com/cldellow/manu/format/IndexTest.java
@@ -1,25 +1,101 @@
 package com.cldellow.manu.format;
 
+import org.junit.After;
 import org.junit.Test;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
 public class IndexTest {
+    private String dbLoc = "/tmp/manu-test.db";
+
+    @After
+    public void cleanup() throws Exception {
+      File f = new File(dbLoc);
+      if(f.exists())
+          f.delete();
+    }
+
+
     @Test
-    public void testEmptyIndexOK() {
+    public void testEmptyIndexOKWhenWritable() throws Exception {
+        Index index = null;
+        try {
+            index = new Index(dbLoc, false);
+        } finally {
+            if(index != null) index.close();
+        }
+    }
+
+    @Test(expected = SQLException.class)
+    public void testEmptyIndexNotOKWhenReadOnly() throws Exception {
+        Index index = null;
+        try {
+            index = new Index(dbLoc, true);
+        } finally {
+            if(index != null) index.close();
+        }
+    }
+
+
+    @Test
+    public void testNonEmptyIndexOK() throws SQLException {
+        Index index = null;
+
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:file:" + dbLoc);
+        try {
+
+            try {
+                index = new Index(dbLoc, true);
+            } finally {
+                if (index != null) index.close();
+            }
+        } finally {
+            conn.close();
+        }
     }
 
     @Test
-    public void testNonEmptyIndexOK() {
-
+    public void testNonExistentKey() throws SQLException {
+        Index index = null;
+        try {
+            index = new Index(dbLoc, false);
+            int id = index.get("this key doesn't exist");
+            assertEquals(-1, id);
+        } finally {
+            if (index != null) index.close();
+        }
     }
 
     @Test
-    public void testNonExistentKey() {
-        //assertTrue(false);
+    public void testAddAndGetKey() throws SQLException {
+        Index index = null;
+        try {
+            index = new Index(dbLoc, false);
+            int id = index.add("somekey");
+            assertNotEquals(-1, id);
+            int id2 = index.get("somekey");
+            assertEquals(id, id2);
+        } finally {
+            if (index != null) index.close();
+        }
     }
 
     @Test
-    public void testAddAndGetKey() {
+    public void testAddExistingKey() throws SQLException {
+        Index index = null;
+        try {
+            index = new Index(dbLoc, false);
+            int id = index.add("somekey");
+            assertNotEquals(-1, id);
+            int id2 = index.add("somekey");
+            assertEquals(id, id2);
+        } finally {
+            if (index != null) index.close();
+        }
     }
 }
