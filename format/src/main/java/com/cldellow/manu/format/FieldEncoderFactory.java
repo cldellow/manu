19588cfae07f1c97263cf0d275a0d1534faa8630
Colin Dellow <cldellow@gmail.com>
2018-01-12 20:09:45 -0500

    remove FEF

--- format/src/main/java/com/cldellow/manu/format/FieldEncoderFactory.java
+++
@@ -1,7 +0,0 @@
-package com.cldellow.manu.format;
-
-public interface FieldEncoderFactory {
-    public FieldType getFieldType();
-
-    public FieldEncoder getFieldEncoder(Object[] values, boolean[] nulls);
-}
